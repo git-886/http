0b57583bba33ba3a94326acf7edf7013373543e1
Ubuntu <ubuntu@localhost.localdomain>
2025-05-19 20:06:31 +0800

    [router模块]

---
+++ HttpServer/include/router/RouterHandler.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <memery>
+#include "../http/HttpRequest.h"
+#include "../http/HttpResponse.h"
+
+namespace http
+{
+    namespace router
+    {
+        class RouterHandler
+        {
+            public:
+                virtual ~RouterHandler() = default;
+                virtual void handle(const HttpRequest& req,HttpResponse * resp) = 0;
+        };
+    }
+}
